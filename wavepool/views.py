e0efcc1ea92c737113a865f1ce368293254e6619
mchae <mchae067@gmail.com>
2020-09-22 01:23:02 -0400

    Hide the cover story box if there are no cover stories set; Changed the id name of teasers to match the ID of the parent for more consistency when accessing; Added check to setting the cover story from model, for cases where the app is run without models (tests)

--- wavepool/views.py
+++ wavepool/views.py
@@ -17,7 +17,9 @@ def front_page(request):
 
     # Multiple coverstories can be set, however by design we only display one. 
     cover_stories = NewsPost.objects.filter(is_cover_story=True).order_by('publish_date')
-    cover_story = cover_stories[0]
+    cover_story = None
+    if cover_stories:
+        cover_story = cover_stories[0]
     other_stories = NewsPost.objects.filter(is_cover_story=False).order_by('publish_date')
     top_stories = other_stories[:3]
     archive_stories = other_stories[3:]
