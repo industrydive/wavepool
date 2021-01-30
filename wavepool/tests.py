5d4aff255a5549840f75aceb0a1fd153eb933e80
Robin Varghese <rvarghese@navisite.com>
2021-01-30 00:23:31 -0500

    Fixes #3 test_newspost_page_content modified tests to compare text

--- wavepool/tests.py
+++ wavepool/tests.py
@@ -49,12 +49,14 @@ class NewsPostDetail(TestBase):
         """
         newsposts = NewsPost.objects.all()
         for newspost in newsposts:
+
             page = self.client.get(newspost.url)
             page_html = BeautifulSoup(page.content, 'html.parser')
             rendered_title = page_html.find('h1', {'id': 'newspost-title'}).text
             rendered_body = page_html.find('div', {'id': 'newspost-body'}).text
+            newspost_body = BeautifulSoup(newspost.body, 'html.parser').text
             self.assertEqual(rendered_title, newspost.title)
-            self.assertEqual(self._clean_text(rendered_body), self._clean_text(newspost.body))
+            self.assertEqual(self._clean_text(rendered_body), self._clean_text(newspost_body))
 
     def test_newspost_body_render(self):
         """ Verify that newsposts rendered at their URL do not display raw HTML to the screen
